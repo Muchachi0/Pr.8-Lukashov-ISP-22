1bccf3f32c14fa57e4ad0c8f514c16e82646612b
muchachi <muchachi@MUCHACHI>
2024-12-19 03:05:59 +0300

    Добавьте файлы проекта.

---
+++ Вложенные циклы/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Трехзначные числа с уникальными цифрами:");
+
+for (int hundreds = 1; hundreds <= 9; hundreds++)
+{
+    for (int tens = 0; tens <= 9; tens++)
+    {
+        if (tens != hundreds)
+        {
+            for (int units = 0; units <= 9; units++)
+            {
+                if (units != hundreds && units != tens)
+                {
+                    Console.WriteLine($"{hundreds}{tens}{units}");
+                }
+            }
+        }
+    }
+}
